654222041a3ef1201e7277f63337a87c555fac5d
mattcat1221 <158774626+mattcat1221@users.noreply.github.com>
2024-08-06 18:27:33 -0400

    Your commit message here

--- static/js/script.js
+++ static/js/script.js
@@ -2,6 +2,12 @@
 let data;
 let renderGraph;
 
+var map = L.map('map').setView([30, 20], 1);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
 const init = async () => {
     data = await (await fetch('/api/v1.0/ecological_data')).json();
 
@@ -24,6 +30,15 @@ const init = async () => {
             "United States of America",
             data.find(obj => obj[option] == maxVal).Country
         ];
+
+        let mapkey = '25b794c90fa551d142661753b59631e6';
+
+        //let countryUrl = `http://api.openweathermap.org/geo/1.0/direct?q=London&limit=1&appid=${mapkey}`
+        //let countryUrl = `http://api.openweathermap.org/geo/1.0/direct?q=United%20States%20of%20America&limit=1&appid=${mapkey}`
+
+        fetch(countryUrl).then(data => data.json()).then(data => {
+            console.log(data);
+        })
 
         selValues = [
             minVal,
@@ -49,4 +64,3 @@ const init = async () => {
 
 }
 
-init();
